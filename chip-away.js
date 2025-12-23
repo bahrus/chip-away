f214b2fc174cf893f5f8a453250422e3e0968c28
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-12-23 12:39:43 -0500

    0.0.1

--- chip-away.js
+++ chip-away.js
@@ -88,13 +88,14 @@ export class ChipAway extends O {
   createChipElement(self, select, option) {
     
     const label = document.createElement('label');
+    //label.classList.add('chip-remove-option-container');
+    label.part.add('chip-remove-option-container');
     const button = document.createElement('button');
     button.type = 'button';
+    //button.classList.add('chip-remove-option-trigger');
+    button.part.add('chip-remove-option-trigger');
+    button.ariaLabel = 'Remove';
     button.innerHTML = '&#10006;';
-    button.style.padding = '8px 12px';
-    button.style.minWidth = '32px';
-    button.style.minHeight = '32px';
-    button.style.cursor = 'pointer';
     this.#chipToOptionRefs.set(button, option);
     button.addEventListener('click', this);
 
